e6d957e8036d1be0770e214001196b5ff0964ae6
MagiSystem1895 <1450079273@qq.com>
2019-12-03 21:45:22 +0800

    添加小虫子进入（上一个错了）

--- src/com/magi/fly/Ant.java
+++ src/com/magi/fly/Ant.java
@@ -1,5 +1,8 @@
 package com.magi.fly;
 
 public class Ant extends Object_Fly{
-
+	
+	Ant(){
+		
+	}
 }
--- src/com/magi/fly/Game.java
+++ src/com/magi/fly/Game.java
@@ -2,20 +2,26 @@ package com.magi.fly;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import javax.swing.JPanel;
 
 public class Game extends JPanel{
-
+	
+	int flying_cnt = 0;//飞行物个数
+	
 	//设置游戏状态
 	int state = 0;
 	public final int START = 0;
 	public final int RUNNING = 1;
 	public final int PAUSE = 0;
 	public final int GAME_OVER = 0;
-		
+	
+	List<Object_Fly> object_Flies = new ArrayList<>();.//储存飞行物的ArrayList
+	
 	public void play() {
 		//1.覆写鼠标适配器的方法
 		//2.添加鼠标监听对象
@@ -61,10 +67,30 @@ public class Game extends JPanel{
 		Timer timer = new Timer();
 		TimerTask timerTask = new TimerTask() {
 			public void run() {
-				System.out.println("正在捕捉动作");
+				//1.飞行物入场
+				//2.移动
+				//3.射击
+				//4.子弹打飞行物
+				//5.删除越界飞行物和子弹
+				//6.检查结束
+				enterAction();
 			}
 		};
 		timer.schedule(timerTask, 1000,1000);
 	}
 	//play方法到此结束
+	private void enterAction() {
+		//1.判断飞行物数量
+		//2.创建飞行物对象
+		//3.将对象加入到ArrayList末尾
+		//flying_cnt:飞行物个数
+		//objed_Filese:飞行物集合
+		if(flying_cnt % 40 == 0) {
+			flying_cnt++;
+			Object_Fly ant = new Ant();
+			object_Flies.add(ant);
+		}
+	}
+	
 }
+//Game类到此结束
